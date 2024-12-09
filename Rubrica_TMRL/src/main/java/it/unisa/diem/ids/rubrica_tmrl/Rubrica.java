fdf573cdbba8fb4ba21af63b61356311473d62c3
diducc <davideruocco2003@gmail.com>
2024-12-09 20:59:33 +0100

    Merge origin/main into main
    Conflicts:
    	Rubrica_TMRL/Rubrica.vcf

--- Rubrica_TMRL/src/main/java/it/unisa/diem/ids/rubrica_tmrl/Rubrica.java
+++ Rubrica_TMRL/src/main/java/it/unisa/diem/ids/rubrica_tmrl/Rubrica.java
@@ -31,12 +31,14 @@ public class Rubrica extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         
+        
         fileDefault = "src\\main\\resources\\it\\unisa\\diem\\ids\\rubrica_tmrl\\Rubrica.vcf";
         
-        Parent root = FXMLLoader.load(getClass().getResource("RubricaMain.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("src\\main\\resources\\it\\unisa\\diem\\ids\\rubrica_tmrl\\RubricaMain.fxml"));
         
         Image icona = new Image("src\\main\\resources\\it\\unisa\\diem\\ids\\rubrica_tmrl\\icona.png");
         stage.getIcons().add(icona);
+        
         
         Scene scene = new Scene(root);
         
